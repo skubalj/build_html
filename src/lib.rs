d5b7cebed79b11a8cc3e804850d8ec6fb0962c69
Joseph Skubal <skubalj@protonmail.com>
2020-12-25 13:26:02 -0600

    Moves container content into lib.rs, rather than a submodule, to make importing easier

--- src/lib.rs
+++ src/lib.rs
@@ -38,13 +38,11 @@
 //! ```
 //!
 
-use content::HeadContent;
+use attributes::Attributes;
+use content::{BodyContent, HeadContent};
 use std::fmt::{self, Display};
 
-pub use containers::{Container, ContainerType, HtmlContainer};
-
 mod attributes;
-pub mod containers;
 mod content;
 
 /// An element that can be converted to HTML
@@ -53,11 +51,82 @@ pub trait Html: fmt::Debug {
     fn to_html_string(&self) -> String;
 }
 
+/// An HTML element that can contain other HTML elements
+///
+/// HTML containers include tags such as: `article`, `div`, `ol`, `ul`.
+pub trait HtmlContainer: Html + Sized {
+    /// Adds the specified HTML element to this container
+    fn add_html(self, html: Box<dyn Html>) -> Self;
+
+    /// Nest the specified container within this container
+    fn add_container(self, container: Container) -> Self {
+        self.add_html(Box::new(container))
+    }
+
+    /// Adds a header tag with the designated level to this container
+    fn add_header(self, level: u8, text: &str) -> Self {
+        let content = BodyContent::Header {
+            level,
+            content: text.into(),
+            attr: Attributes::empty(),
+        };
+        self.add_html(Box::new(content))
+    }
+
+    /// Adds an `<img>` tag to this container
+    fn add_image(self, src: &str, alt: &str) -> Self {
+        let content = BodyContent::Image {
+            src: src.into(),
+            alt: alt.into(),
+            attr: Attributes::empty(),
+        };
+        self.add_html(Box::new(content))
+    }
+
+    /// Adds an `<a>` tag to this container
+    fn add_link(self, href: &str, text: &str) -> Self {
+        let content = BodyContent::Link {
+            href: href.into(),
+            content: text.into(),
+            attr: Attributes::empty(),
+        };
+        self.add_html(Box::new(content))
+    }
+
+    /// Adds a `<p>` tag element to this Container
+    fn add_paragraph(self, text: &str) -> Self {
+        let content = BodyContent::Paragraph {
+            content: text.into(),
+            attr: Attributes::empty(),
+        };
+        self.add_html(Box::new(content))
+    }
+
+    /// Adds a `<pre>` tag element to this container
+    fn add_preformatted(self, text: &str) -> Self {
+        let content = BodyContent::Preformatted {
+            content: text.into(),
+            attr: Attributes::empty(),
+        };
+        self.add_html(Box::new(content))
+    }
+}
+
 /// This struct represents an entire page of HTML which can built up by chaining addition methods.
+/// 
 /// This creates an effect similar to the [Decorator Pattern](https://en.wikipedia.org/wiki/Decorator_pattern)
 ///
 /// To convert an `HtmlPage` to a [`String`] which can be sent back to a client, use the
 /// [`Html::to_html_string()`] method
+/// 
+/// # Example
+/// ```
+/// # use html_gen::*; 
+/// let page: String = HtmlPage::new()
+///     .add_title("My Page")
+///     .add_header(1, "Header Text")
+///     .to_html_string();
+/// ```
 #[derive(Debug)]
 pub struct HtmlPage {
     head: Vec<Box<dyn Html>>,
@@ -122,6 +191,86 @@ impl HtmlPage {
     }
 }
 
+/// The different types of Html Containers that can be added to the page
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ContainerType {
+    Article,
+    Div,
+    Main,
+    OrderedList,
+    UnorderedList,
+}
+
+impl Display for ContainerType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ContainerType::Article => write!(f, "article"),
+            ContainerType::Div => write!(f, "div"),
+            ContainerType::Main => write!(f, "main"),
+            ContainerType::OrderedList => write!(f, "ol"),
+            ContainerType::UnorderedList => write!(f, "ul"),
+        }
+    }
+}
+
+/// A container for HTML elements.
+///
+/// As the name would suggest, a `Container` contains other HTML elements. This struct guarantees
+/// that the elements added will be converted to HTML strings in the same order as they were
+/// added.
+///
+/// Supported container types are provided by the [`ContainerType`] enum. This struct is what
+/// allows Lists (`<ol>` / `<ul>`) as well as `<div>`s to be added to the `HtmlPage`
+#[derive(Debug)]
+pub struct Container {
+    tag: ContainerType,
+    elements: Vec<Box<dyn Html>>,
+    attr: Attributes,
+}
+
+impl Html for Container {
+    fn to_html_string(&self) -> String {
+        let content = match self.tag {
+            ContainerType::OrderedList | ContainerType::UnorderedList => self
+                .elements
+                .iter()
+                .map(|item| format!("<li>{}</li>", item.to_html_string()))
+                .fold(String::new(), |acc, next| acc + &next),
+            _ => self
+                .elements
+                .iter()
+                .map(|item| item.to_html_string())
+                .fold(String::new(), |acc, next| acc + &next),
+        };
+
+        format!("<{}{}>{}</{}>", self.tag, self.attr, content, self.tag)
+    }
+}
+
+impl HtmlContainer for Container {
+    fn add_html(mut self, content: Box<dyn Html>) -> Self {
+        self.elements.push(content);
+        self
+    }
+}
+
+impl Default for Container {
+    fn default() -> Self {
+        Container::new(ContainerType::Div)
+    }
+}
+
+impl Container {
+    /// Creates a new container with the specified tag.
+    pub fn new(tag: ContainerType) -> Self {
+        Container {
+            tag,
+            elements: Vec::new(),
+            attr: Attributes::empty(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -142,6 +291,33 @@ mod tests {
                 html_string,
                 "<!DOCTYPE html><html><head></head><body></body></html>"
             )
+        }
+    }
+
+    mod container {
+        use super::*;
+        use test_case::test_case;
+
+        #[test_case(Container::new(ContainerType::Div), "rust-lang.org", "rust", r#"<div><a href="rust-lang.org">rust</a></div>"#; "test_div")]
+        #[test_case(Container::new(ContainerType::OrderedList), "abc", "123", r#"<ol><li><a href="abc">123</a></li></ol>"#; "test_ordered_list")]
+        #[test_case(Container::new(ContainerType::UnorderedList), "abc", "123", r#"<ul><li><a href="abc">123</a></li></ul>"#; "test_unordered_list")]
+        fn test_add_link(container: Container, href: &str, text: &str, expected: &str) {
+            // Act
+            let actual = container.add_link(href, text).to_html_string();
+
+            // Assert
+            assert_eq!(actual, expected);
+        }
+
+        #[test_case(Container::new(ContainerType::Div), "hello world", "<div><p>hello world</p></div>"; "test_div")]
+        #[test_case(Container::new(ContainerType::OrderedList), "hello world", "<ol><li><p>hello world</p></li></ol>"; "test_ordered_list")]
+        #[test_case(Container::new(ContainerType::UnorderedList), "hello world", "<ul><li><p>hello world</p></li></ul>"; "test_unordered_list")]
+        fn test_add_paragraph(container: Container, text: &str, expected: &str) {
+            // Act
+            let actual = container.add_paragraph(text).to_html_string();
+
+            // Assert
+            assert_eq!(actual, expected);
         }
     }
 }
