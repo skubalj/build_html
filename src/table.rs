774864be913489b719f929d04f9dda8355bc9a81
Joseph Skubal <skubalj@protonmail.com>
2021-01-23 20:47:58 -0600

    Additional implementation of Tables, Documentation

--- src/table.rs
+++ src/table.rs
@@ -1,3 +1,8 @@
+//! This module contains the implementations used to add HTML tables to this library
+//! 
+//! Tables are provided using the `Table` struct, and are loaded from 1 and 2D data
+//! structures which implement the `IntoIterator` struct
+
 use crate::attributes::Attributes;
 use crate::Html;
 use std::collections::HashMap;
@@ -10,15 +15,48 @@ where
     T::Item: Display,
 {
     row.into_iter()
-        .map(|element| format!("<{tag}>{content}</{tag}>", tag = cell_tag, content = element))
+        .map(|element| {
+            format!(
+                "<{tag}>{content}</{tag}>",
+                tag = cell_tag,
+                content = element
+            )
+        })
         .chain(std::iter::once("</tr>".into()))
         .fold(String::from("<tr>"), |a, n| a + &n)
 }
 
 /// Represents an HTML `<table>` element with all its children.
 ///
-/// The easiest way to make a table is by simply passing in a table:
+/// The easiest way to make a table is by simply passing in a 2D Array or `Vec`.
+/// Using this method, the entire contents will be placed in `<td>` elements within
+/// the `<tbody>`. If a header row is desired, one can be added manually.
 ///
+/// # Example
+/// ```
+/// # use html_gen::*;
+/// let source_table = [
+///     [1, 2, 3],
+///     [4, 5, 6],
+///     [7, 8, 9]
+/// ];
+/// let html_table = Table::from(&source_table)
+///     .add_header_row(&['A', 'B', 'C'])
+///     .to_html_string();
+///
+/// assert_eq!(
+///     html_table,
+///     concat!(
+///         "<table><thead>",
+///         "<tr><th>A</th><th>B</th><th>C</th></tr>",
+///         "</thead><tbody>",
+///         "<tr><td>1</td><td>2</td><td>3</td></tr>",
+///         "<tr><td>4</td><td>5</td><td>6</td></tr>",
+///         "<tr><td>7</td><td>8</td><td>9</td></tr>",
+///         "</tbody></table>"
+///     )
+/// );
+/// ```
 #[derive(Debug)]
 pub struct Table {
     thead: Vec<String>,
@@ -44,14 +82,12 @@ where
     <<T as std::iter::IntoIterator>::Item as IntoIterator>::Item: Display,
 {
     fn from(source: T) -> Self {
-        let mut iterator = source.into_iter();
-        let header_row = match iterator.next() {
-            Some(row) => parse_table_row(row, "th"),
-            None => return Table::new(), // Return an empty table if the source is empty
-        };
-        let body_rows = iterator.map(|row| parse_table_row(row, "td")).collect();
+        let body_rows = source
+            .into_iter()
+            .map(|row| parse_table_row(row, "td"))
+            .collect();
         Table {
-            thead: vec![header_row],
+            thead: Vec::new(),
             tbody: body_rows,
             attr: Attributes::default(),
         }
@@ -59,6 +95,7 @@ where
 }
 
 impl Table {
+    /// Creates a new table with an empty header and body
     pub fn new() -> Self {
         Table {
             thead: Vec::new(),
@@ -89,6 +126,64 @@ impl Table {
         self.attr = Attributes::from(attributes);
         self
     }
+
+    /// Adds the specified row to the table header 
+    /// 
+    /// Note that no checking is done to ensure that the row is of the proper length
+    /// 
+    /// # Example
+    /// ```
+    /// # use html_gen::*;
+    /// let table = Table::new()
+    ///     .add_header_row(vec!["Mon", "Tues", "Wed", "Thurs", "Fri"])
+    ///     .to_html_string();
+    /// 
+    /// assert_eq!(
+    ///     table, 
+    ///     concat!(
+    ///         "<table><thead>",
+    ///         "<tr><th>Mon</th><th>Tues</th><th>Wed</th><th>Thurs</th><th>Fri</th></tr>",
+    ///         "</thead><tbody></tbody></table>"
+    ///     )
+    /// )
+    /// ```
+    pub fn add_header_row<T>(mut self, row: T) -> Self
+    where
+        T: IntoIterator,
+        T::Item: Display,
+    {
+        self.thead.push(parse_table_row(row, "th"));
+        self
+    }
+
+    /// Adds the specified row to the table body 
+    /// 
+    /// Note that no checking is done to ensure that the row is of the proper length
+    /// 
+    /// # Example
+    /// ```
+    /// # use html_gen::*;
+    /// let table = Table::new()
+    ///     .add_body_row(vec![1, 2, 3, 4, 5])
+    ///     .to_html_string();
+    /// 
+    /// assert_eq!(
+    ///     table, 
+    ///     concat!(
+    ///         "<table><thead></thead><tbody>",
+    ///         "<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>",
+    ///         "</tbody></table>"
+    ///     )
+    /// )
+    /// ```
+    pub fn add_body_row<T>(mut self, row: T) -> Self
+    where
+        T: IntoIterator,
+        T::Item: Display,
+    {
+        self.tbody.push(parse_table_row(row, "td"));
+        self
+    }
 }
 
 #[cfg(test)]
@@ -107,9 +202,8 @@ mod tests {
         assert_eq!(
             result,
             concat!(
-                "<table><thead>",
-                "<tr><th>1</th><th>2</th><th>3</th></tr>",
-                "</thead><tbody>",
+                "<table><thead></thead><tbody>",
+                "<tr><td>1</td><td>2</td><td>3</td></tr>",
                 "<tr><td>4</td><td>5</td><td>6</td></tr>",
                 "<tr><td>7</td><td>8</td><td>9</td></tr>",
                 "</tbody></table>"
@@ -129,9 +223,8 @@ mod tests {
         assert_eq!(
             result,
             concat!(
-                "<table><thead>",
-                "<tr><th>1</th><th>2</th><th>3</th></tr>",
-                "</thead><tbody>",
+                "<table><thead></thead><tbody>",
+                "<tr><td>1</td><td>2</td><td>3</td></tr>",
                 "<tr><td>4</td><td>5</td><td>6</td></tr>",
                 "<tr><td>7</td><td>8</td><td>9</td></tr>",
                 "</tbody></table>"
